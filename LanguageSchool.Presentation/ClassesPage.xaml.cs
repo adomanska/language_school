44132b77096fe5349dae9b4e36a5fd3a3455b7ee
Andżelika Domańska <adomanska96@gmail.com>
2017-10-26 12:47:35 +0200

    Add start page and pages switching

---
+++ LanguageSchool.Presentation/ClassesPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LanguageSchool.Presentation
+{
+    /// <summary>
+    /// Interaction logic for ClassesPage.xaml
+    /// </summary>
+    public partial class ClassesPage : Page
+    {
+        public ClassesPage()
+        {
+            InitializeComponent();
+        }
+        private void goToStartPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("StartPage.xaml", UriKind.Relative));
+        }
+    }
+}
