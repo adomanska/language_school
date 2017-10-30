36b8877d82c631b0a2713d07f1b13c791d8dc071
Andżelika Domańska <adomanska96@gmail.com>
2017-10-30 15:10:39 +0100

    Read existing languages from JSON

--- LanguageSchool.BusinessLogic/LanguageBLL.cs
+++ LanguageSchool.BusinessLogic/LanguageBLL.cs
@@ -5,16 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using LanguageSchool.Model;
 using LanguageSchool.DataAccess;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace LanguageSchool.BusinessLogic
 {
     public class LanguageBLL
     {
         private LanguageDAL languageDAL;
+        JArray existingLanguages;
 
         public LanguageBLL(LanguageSchoolContext context)
         {
             languageDAL = new LanguageDAL(context);
+            existingLanguages = JArray.Parse(@File.ReadAllText("LanguagesList.json"));
         }
         public List<Language> GetAll()
         {
@@ -29,6 +33,8 @@ namespace LanguageSchool.BusinessLogic
         }
         public void Add(string languageName)
         {
+            if (!existingLanguages.Values().Contains(languageName))
+                throw new Exception("Language doesn't exist");
             Language language = new Language { LanguageName = languageName };
             try
             {
